6b555939d93708a63adeaf5f92db2e66e990f0de
machosen <161610465+machosen@users.noreply.github.com>
2024-04-17 11:33:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,131 @@
+const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".botao");
+console.log(botoes);
+const botoes = document.querySelectorAll(".botao");
+
+for()
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;)
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    console.log();
+}
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick
+    console.log();
+}
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        botoes[i].classList.add("ativo");
+    }
+    console.log();
+}
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+    }
+    console.log();
+}
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+    }
+}
+const botoes = document.querySelectorAll(".botao");
+console.log(botoes)
+for(let i=0; i <  botoes.length; i++){
+    console.log(i);
+}
+botoes[i].onclick = function(){
+    
+    botoes[i].classList.add("ativo");
+}
+for(let j=0;j<botoes.length;j++){
+    botoes[j].classList.remove("ativo");
+}
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+    }
+}
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+//Código omitido
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+//Código omitido
+
+const contadores = document.querySelectorAll(".contador");
+
+//Código omitido
+//Código omitido
+
+const contadores = document.querySelectorAll(".contador");
+
+contadores[0].textContent = "Contagem regressiva";
+//Código omitido
+
+const contadores = document.querySelectorAll(".contador"); 
+const tempoObjetivo1 = new Date("2024-10-05"); 
+
+contadores[0].textContent = tempo0bjetivo1; 
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
